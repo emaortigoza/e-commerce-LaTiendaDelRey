880e9f31c9e65d4430b1dfda36457abd59d9caaf
emaOrtigoza <emaortigoza@gmail.com>
2023-04-22 15:25:09 -0300

    Se trabajo agregando los productos al itemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import mFetch from "../../utils/mFetch"
+import { Button, Card, Col, Container, Row } from "react-bootstrap"
+import './ItemListContainer.css'
+
 
 function ItemListContainer({greeting}) {
+    const[productos, setProductos] = useState([])
+    const [loading, setloading] = useState(true)
+
+    useEffect(() =>{
+      mFetch()
+      .then( resultado => {
+        setProductos(resultado)
+      })
+      .catch( error => console.log(error))
+      .finally(()=> setloading(false))
+    },[])
+
   return (
-    <div className='text-center'>
-        <h2>{greeting}</h2>
-    </div>
+    <Container fluid className="contenedor">
+      <Row className="mt-5 catalogo">
+            { loading ? <h2>Cargando...</h2>
+                      :
+                       productos.map( producto => 
+                                                <Col className="col-xl-3 columnas">
+                                                <Card className="card" key={producto.id} style={{ width: '18rem' }}>
+                                                    <Card.Img className="img" variant="top" src={producto.foto} alt={producto.nombre} />
+                                                    <Card.Body>
+                                                      <Card.Title>{producto.nombre}</Card.Title>
+                                                      <Card.Text>
+                                                        {producto.categoria}
+                                                        <br />
+                                                        Some quick example text to build on the card title and make up the
+                                                         bulk of the card's content.
+                                                      </Card.Text>
+                                                      <Card.Text>$ {producto.precio}</Card.Text>
+                                                     <Button text-center variant="primary">Detalle</Button>
+                                                    </Card.Body>
+                                                  </Card>
+                                                </Col>
+                                                  )  
+
+            } 
+      </Row>
+    </Container>
+   
   )
 }
 
